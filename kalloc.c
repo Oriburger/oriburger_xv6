3af12dc58af9791179d85b3b9700fd270378b81c
Oriburger <uss425@naver.com>
2023-11-26 00:22:03 +0900

    reference counter 개념 추가
    - kalloc.c
      - 전역변수 num_free_pages, pgrefcount 추가
      - num_free_pages 관련
        - kinit1 -> 초기화
        - kalloc -> 감소,  Kfree -> 증가
      - pgrefcount 관련
        - get_refcount, inc_refcount, dec_refcount 추가
        - freerange() -> pgrefcount 0으로 초기화
        - kfree() -> dec_refcount 추가
        - kalloc() -> pgrefcount 1로 설정

--- kalloc.c
+++ kalloc.c
@@ -9,9 +9,6 @@
 #include "mmu.h"
 #include "spinlock.h"
 
-uint num_free_pages;
-uint pgrefcount[PHYSTOP >> PGSHIFT];
-
 void freerange(void *vstart, void *vend);
 extern char end[]; // first address after kernel loaded from ELF file
                    // defined by the kernel linker script in kernel.ld
@@ -26,6 +23,9 @@ struct {
   struct run *freelist;
 } kmem;
 
+uint num_free_pages;
+uint pgrefcount[PHYSTOP >> PGSHIFT];
+
 // Initialization happens in two phases.
 // 1. main() calls kinit1() while still using entrypgdir to place just
 // the pages mapped by entrypgdir on free list.
@@ -36,7 +36,7 @@ kinit1(void *vstart, void *vend)
 {
   initlock(&kmem.lock, "kmem");
   kmem.use_lock = 0;
-  num_free_pages = 0;
+  num_free_pages = 0;  //FREE PAGES 개수 초기화@@@@@@@@@@@@@
   freerange(vstart, vend);
 }
 
@@ -53,7 +53,10 @@ freerange(void *vstart, void *vend)
   char *p;
   p = (char*)PGROUNDUP((uint)vstart);
   for(; p + PGSIZE <= (char*)vend; p += PGSIZE)
+  {
     kfree(p);
+    pgrefcount[V2P(p)] = 0; //pgrefcount 0으로 초기화@@@@@@@@@@@@ 
+  }
 }
 //PAGEBREAK: 21
 // Free the page of physical memory pointed at by v,
@@ -68,15 +71,20 @@ kfree(char *v)
   if((uint)v % PGSIZE || v < end || V2P(v) >= PHYSTOP)
     panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(v, 1, PGSIZE);
-
   if(kmem.use_lock)
-    acquire(&kmem.lock);
-  r = (struct run*)v;
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  num_free_pages++;
+      acquire(&kmem.lock);
+
+  if(get_refcount(V2P(v)) > 0)   //0보다 크면, REFCOUNT만 줄임 @@@@@@@@@@@@@
+    dec_refcount(V2P(v));
+
+  if(get_refcount(V2P(v)) == 0) {  //0이면, free해도됨 @@@@@@@@@@@@@
+    // Fill with junk to catch dangling refs.
+    memset(v, 1, PGSIZE);
+    num_free_pages++;  //FREE PAGES 개수 증가 @@@@@@@@@@@@
+    r = (struct run*)v; 
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+  }
   if(kmem.use_lock)
     release(&kmem.lock);
 }
@@ -94,9 +102,10 @@ kalloc(void)
   r = kmem.freelist;
   if(r)
     kmem.freelist = r->next;
-  num_free_pages--;
+  num_free_pages--;  //FREE PAGES 개수 감소 @@@@@@@@@@@@
   if(kmem.use_lock)
     release(&kmem.lock);
+  pgrefcount[V2P(r) >> PGSHIFT] = 1; //0으로 지정 @@@@@@@@@@@@@
   return (char*)r;
 }
 
@@ -104,4 +113,22 @@ int
 get_numfreepages(void)
 {
   return num_free_pages;
+}
+
+uint 
+get_refcount(uint pa)
+{
+  return pgrefcount[pa >> PGSHIFT];
+}
+
+uint
+inc_refcount(uint pa)
+{
+  return ++pgrefcount[pa >> PGSHIFT];
+}
+
+uint
+dec_refcount(uint pa)
+{
+  return --pgrefcount[pa >> PGSHIFT];
 }
